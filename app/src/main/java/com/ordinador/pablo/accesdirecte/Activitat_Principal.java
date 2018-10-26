dd537f0ae870c3d7f43409eab9ae12e134ac1ef4
Pablo Fraile <socdelbarca72@gmail.com>
2018-10-26 14:29:13 +0200

    26/10/18 actualització
    Actualitza el projecte

--- app/src/main/java/com/ordinador/pablo/accesdirecte/Activitat_Principal.java
+++ app/src/main/java/com/ordinador/pablo/accesdirecte/Activitat_Principal.java
@@ -1,24 +1,41 @@
 package com.ordinador.pablo.accesdirecte;
 
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View; // Classe necessària per a els elements d'interfície d'usuari
+
 import android.content.Intent; // Classe necessària per a executar tasques
+import android.content.Context; // Classe necessària per a saber el contexte de la app
+
 import android.net.Uri; // Classe necessària per obrir la galeria
 import android.content.SharedPreferences; // Classe necessària per a guardar els paràmetres del canvi de color,
+
 import android.view.Menu; // Classe necessària per a dir que hi ha un menú
 import android.view.MenuInflater; // Classe necessària per a dir quin es l'arxiu xml del menú.
 import android.view.MenuItem; // Classe necessària per a programar la funció dels diferents ítems del xml menú
+
 import android.widget.Button; // Classe necessària per als botons
-import android.provider.MediaStore; // Classe necessària per a executar la app de càmera.
-import android.view.View; // Classe necessària per a els elements d'interfície d'usuari
-import android.widget.Toast; // Classe necessària per a executar missatges tipus Toast.
 import android.widget.ToggleButton; // Classe Necessària per a controlar els botons tipus toggle
 import android.widget.CompoundButton; // Classe necessària per a controlar un botó amb dos estats (encés o no encés) com per exemple el Toggle Button
-import android.content.Context; // Classe necessària per a saber el contexte de la app
+
 import android.net.wifi.WifiManager; // Classe necessària per a controlar el wifi de el SmartPhone
 import android.net.ConnectivityManager; // Classe que respon a consultes sobre l'estat de la connectivitat de xarxa
 import android.net.NetworkInfo; // Classe que describeix l'estat de una interfície de xarxa
+import android.net.Network; // Classe necessària per al control de la xarxa
+
+import android.provider.MediaStore; // Classe necessària per a executar la app de càmera.
+
+import android.widget.Toast; // Classe necessària per a executar missatges tipus Toast.
+
+import android.provider.Settings; // Classe necessària per a redireccionar a la aplicació configuració.
+import android.os.Build; // Classe necessària per a mirar la API del dispositiu
+
+
 
 public class Activitat_Principal extends AppCompatActivity {
 
@@ -28,14 +45,27 @@ public class Activitat_Principal extends AppCompatActivity {
         startActivity(i);
     }
 
-    // Métode per a obrir la Activity Opcions de la app
+    // Métode per obrir la Activity Opcions de la app
     public void conf(){
         Intent conf = new Intent(this,configuracio.class);
         startActivity(conf);
     }
 
+    // Métode per a fer check de la API de Android Actual ( necessari per a móbils que no tenen la API 28 (Android P))
+    public void api(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Intent i = new Intent(Settings.ACTION_DATA_USAGE_SETTINGS);
+            startActivity(i);
+        }
+        else {
+            Toast.makeText(this, "Your Android Version doesn't support that feature",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Flashlight
+    private String mCameraId;
+
     // Métode per a mostrar el menú
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
@@ -43,7 +73,6 @@ public class Activitat_Principal extends AppCompatActivity {
     }
 
     // Métode per a programar el Menú
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
@@ -153,13 +182,15 @@ public class Activitat_Principal extends AppCompatActivity {
         Button whatsapp;
         whatsapp = findViewById(R.id.botowhatsapp);
 
-        ToggleButton toggle = findViewById(R.id.togglewifi);
-        ConnectivityManager conectat = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        assert conectat != null;
-        NetworkInfo wifi = conectat.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (wifi.isConnected()){
-            toggle.setChecked(true);
-        }
+        Button datos;
+        datos = findViewById(R.id.btdatos);
+
+        ToggleButton wifitoggle;
+        wifitoggle = findViewById(R.id.togglewifi);
+
+        ToggleButton llanternatoggle;
+        llanternatoggle = findViewById(R.id.togglellanterna);
+
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -197,18 +228,62 @@ public class Activitat_Principal extends AppCompatActivity {
                 whatsapp();
             }
         });
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+        // Mira si el dispositiu està ja conectat a la xarxa i el tipus de conexió en que es troba
+        final ConnectivityManager connexio = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        for (Network network : connexio.getAllNetworks()) {
+            NetworkInfo networkInfo = connexio.getNetworkInfo(network);
+            if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                wifitoggle.setChecked(true);
+            }
+        }
+        // Configura el toggle wifi
+        wifitoggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
                     WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    assert wifi != null;
                     wifi.setWifiEnabled(true);
                 }
                 else{
                     WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    assert wifi != null;
                     wifi.setWifiEnabled(false);
+                }
+            }
+        });
+
+        // Configura el botó de Dades Mòbils (Redirecciona a configuració/dades movils), únicament disponible per a la API de Android 28 (Android P)
+        datos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                api();
+            }
+        });
+
+        // Flashlight
+         final CameraManager mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+         try {
+             mCameraId = mCameraManager.getCameraIdList()[0];
+         }
+         catch (CameraAccessException e){
+             e.printStackTrace();
+         }
+
+        // Configura la llanterna
+        llanternatoggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    try {
+                        mCameraManager.setTorchMode(mCameraId, true);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+
                 }
             }
         });
